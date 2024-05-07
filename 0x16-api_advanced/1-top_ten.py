621b8142e564a0c37d7dacb03009fed6c7af56ec
Solomon Aberra <solomonaberra75@gmail.com>
2024-05-07 03:13:41 -0700

    0x16. API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and prints
-the titles of the first 10 hot posts """
+"""Script that prints out top ten hottest gists
+of a subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """a function that queries the Reddit API and prints
-the titles of the first 10 hot posts """
-    user_agent = "MyRedditBot/1.0"
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": user_agent}
-    response = requests.get(url, headers=headers)
+    """Function that fetches top 10 gists"""
+    apiUrl = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    userAgent = "Mozilla/5.0"
+    limits = 10
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"][:10]
-        for post in posts:
-            title = post["data"]["title"]
-            print(title)
-    elif response.status_code == 404:
-        print("None")
-    else:
-        print(f"Error: {response.status_code}")
-        print("None")
-
+    response = requests.get(
+        apiUrl, headers={"user-agent": userAgent}, params={"limit": limits})
+    if not response:
+        print('None')
+        return
+    response = response.json()
+    list_obj = response['data']['children']
+    for obj in list_obj:
+        print(obj['data']['title'])
+    return
